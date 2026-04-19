887493b3224a5c5e768efff5168e5b30f44aa7af
SUMITRA-202 <4al24cs202@aiet.org.in>
2026-04-19 11:06:22 +0530

    Add files via upload

---
+++ ArrayListProgram3.java
@@ -0,0 +1,20 @@
+package ArrayListProgram;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListProgram3 {
+
+	public static void main(String[] args) {
+		ArrayList<String> colors = new ArrayList<>();
+
+        colors.add("Blue");
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Yellow");
+
+        Collections.sort(colors);
+
+        System.out.println("Sorted Colors: " + colors);
+    }
+}
