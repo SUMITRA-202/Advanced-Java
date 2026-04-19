de9d61c2003dc994aa7621d8a406fdad7a8017db
SUMITRA-202 <4al24cs202@aiet.org.in>
2026-04-19 12:04:22 +0530

    Update LinkedListProgram5.java

--- LinkedListProgram5.java
+++ LinkedListProgram5.java
@@ -1,3 +1,6 @@
+//Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using Collections.swap(l_list, 0, 2))
+
+
 package LinkedListProgram;
 
 import java.util.Collections;
